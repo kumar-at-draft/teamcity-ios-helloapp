84d1beef1aafb207174034cc6d6b3a124a852fb7
Kumar Harsh <hey@kumarharsh.me>
2025-02-14 03:53:59 +0530

    Initial Commit

---
+++ HelloAppTests/HelloAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppTests.swift
+//  HelloAppTests
+//
+//  Created by Kumar Harsh on 14/02/25.
+//
+
+import Testing
+@testable import HelloApp
+
+struct HelloAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
